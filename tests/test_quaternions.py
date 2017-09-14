173aaa34b2bda09a9629ef3d1940913b7a731e18
Matías Graña <matias@satellogic.com>
2017-09-14 13:37:28 -0300

    adapt tests to unittest

--- tests/test_quaternions.py
+++ tests/test_quaternions.py
@@ -1,4 +1,7 @@
 import unittest
+from hypothesis import given
+from hypothesis.strategies import floats
+
 import numpy as np
 
 import quaternions
@@ -118,3 +121,25 @@ class QuaternionTest(unittest.TestCase):
                         expected = 2 * np.pi - expected
 
                     self.assertAlmostEqual(expected, q.distance(q_rot))
+
+
+class ParameterizedTests(unittest.TestCase):
+    @given(floats(min_value=-180, max_value=180),
+           floats(min_value=-90, max_value=90),
+           floats(min_value=0, max_value=360))
+    def test_quat_ra_dec_roll(ra, dec, roll):
+        q = Quaternion.from_ra_dec_roll(ra, dec, 2.)
+        ob_ra, ob_dec, ob_roll = q.ra_dec_roll
+        assert abs(ob_dec - dec) < 1e-8
+        assert abs(ob_ra - ra) < 1e-8
+        
+
+    @given(floats(min_value=-2, max_value=2),
+           floats(min_value=-2, max_value=2),
+           floats(min_value=-2, max_value=2))
+    def test_quat_rotation_vector(rx, ry, rz):
+        rot = np.array([rx, ry, rz])
+        q = Quaternion.from_rotation_vector(rot)
+        distance = np.linalg.norm(rot - q.rotation_vector)
+
+        assert (distance % τ) < 1e-8

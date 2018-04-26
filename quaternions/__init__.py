6a9177aaf173b56b8b74ff697105a336e155a138
Juan Luis Cano Rodríguez <juanlu@satellogic.com>
2018-04-26 17:18:38 +0200

    Use versioneer

--- quaternions/__init__.py
+++ quaternions/__init__.py
@@ -1,2 +1,6 @@
 from quaternions.quaternion import Quaternion  # NOQA
 from quaternions.general_quaternion import GeneralQuaternion, QuaternionError  # NOQA
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
